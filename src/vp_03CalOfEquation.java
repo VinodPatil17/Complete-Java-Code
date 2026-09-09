8146a1636895e4373b45fac773fdc60cddd16afb
Vinod <brndxdvl@gmail.com>
2026-09-09 17:47:50 +0530

    calculation of equation

---
+++ src/vp_03CalOfEquation.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class vp_03CalOfEquation {
+    public static void main(String[] args){
+        float v = 10;
+        float u = 20;
+        float a = 5;
+        float s = 4;
+
+        float sum = (v*v-u*u) / (2*a*s);
+        System.out.println(sum);
+    }
+}
