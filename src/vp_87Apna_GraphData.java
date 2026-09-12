38e1cb0a753f52d165a1dbcb3b19a1c627e18ad8
Vinod <brndxdvl@gmail.com>
2026-09-12 18:46:46 +0530

    creating graph data structure using Adjacency List

--- src/vp_87Apna_GraphData.java
+++ src/vp_87Apna_GraphData.java
@@ -43,6 +43,7 @@ public class vp_87Apna_GraphData{
         for(int i = 0; i < graph[2].size(); i++){
             Edge e = graph[2].get(i);
             System.out.println(e.src + " "+ e.dst);
+
         }
     }
 }
